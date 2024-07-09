492be76ced61e25391593b46aad5c1f25ef4211c
PiPo <pipo.khomgrid@gmail.com>
2024-07-09 11:47:53 +0700

    create register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
 import "./App.css";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-red-500 ">Hello world!</h1>
-  );
+  return <RegisterPage />;
 }
 
 export default App;
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { registerSchema } from "../schemas/schema";
+import { CustomInput } from "../componants/CustomInput";
+import axios from "axios";
+
+function RegisterPage() {
+  async function onSubmit(values, actions) {
+    // console.log(values);
+    // console.log(actions);
+    try {
+      await axios.post("http://localhost:4000/auth/register", values);
+    } catch {
+      actions.resetForm();
+    }
+  }
+  return (
+    <section className="w-full h-screen flex flex-col items-center justify-center">
+      <div className="w-[380px] h-[512px] flex flex-col items-center justify-center gap-[40px]">
+        <h1 className="w-[121px] h-[44px] text-[36px] font-bold text-white">
+          Register
+        </h1>
+        <Formik
+          initialValues={{ name: "", email: "", password: "" }}
+          validationSchema={registerSchema}
+          onSubmit={onSubmit}
+        >
+          {({ isSubmitting }) => (
+            <Form className="flex flex-col justify-center items-center gap-[24px]">
+              <div className="flex flex-col gap-[4px]">
+                <CustomInput
+                  label="Name"
+                  name="name"
+                  type="text"
+                  placeholder="Full name"
+                />
+                <CustomInput
+                  label="Email"
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                />
+                <CustomInput
+                  label="Password"
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                />
+              </div>
+              <button
+                disabled={isSubmitting}
+                className="btn btn-primary h-[48px] w-[383px] bg-[color:hsla(223, 82%, 62%, 1)]]"
+                type="submit"
+              >
+                Register
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </section>
+  );
+}
+
+export default RegisterPage;
