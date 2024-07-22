1b70ebd3a071ab98e289169bc6ab429da787df9d
Kanokphit <kanokphit55@gmail.com>
2024-07-22 16:20:17 +0700

    fix: edit fillOpacity and strokeWidth to navbar user icon

--- client/src/componants/NavbarUser.jsx
+++ client/src/componants/NavbarUser.jsx
@@ -151,7 +151,7 @@ function NavbarUser() {
                       <path
                         d="M19.7274 20.4471C19.2716 19.1713 18.2672 18.0439 16.8701 17.2399C15.4729 16.4358 13.7611 16 12 16C10.2389 16 8.52706 16.4358 7.12991 17.2399C5.73276 18.0439 4.72839 19.1713 4.27259 20.4471"
                         stroke="white"
-                        stroke-width="1.2"
+                        strokeWidth="1.2"
                         strokeLinecap="round"
                       />
                       <circle
@@ -159,9 +159,9 @@ function NavbarUser() {
                         cy="8"
                         r="4"
                         fill="#7E869E"
-                        fill-opacity="0.25"
+                        fillOpacity="0.25"
                         stroke="white"
-                        stroke-width="1.2"
+                        strokeWidth="1.2"
                         strokeLinecap="round"
                       />
                     </svg>
@@ -299,7 +299,7 @@ function NavbarUser() {
                     <path
                       d="M19.7274 20.4471C19.2716 19.1713 18.2672 18.0439 16.8701 17.2399C15.4729 16.4358 13.7611 16 12 16C10.2389 16 8.52706 16.4358 7.12991 17.2399C5.73276 18.0439 4.72839 19.1713 4.27259 20.4471"
                       stroke="white"
-                      stroke-width="1.2"
+                      strokeWidth="1.2"
                       strokeLinecap="round"
                     />
                     <circle
@@ -307,9 +307,9 @@ function NavbarUser() {
                       cy="8"
                       r="4"
                       fill="#7E869E"
-                      fill-opacity="0.25"
+                      fillOpacity="0.25"
                       stroke="white"
-                      stroke-width="1.2"
+                      strokeWidth="1.2"
                       strokeLinecap="round"
                     />
                   </svg>
