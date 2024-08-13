a0f71443141f81f1b6a05ec54f60ca4688554997
Kanokphit <kanokphit55@gmail.com>
2024-08-13 23:28:42 +0700

    fix:edit responsive design into MovieList

--- client/src/componants/homepage/MovieList.jsx
+++ client/src/componants/homepage/MovieList.jsx
@@ -80,18 +80,15 @@ function MovieList() {
             <ClockLoader color="#4f7cee" />
           </div>
         ) : (
-          <div className="grid grid-cols-2  xl:grid-cols-4 gap-[20px]">
+          <div className="grid grid-cols-2 h-fit xl:grid-cols-4 gap-[20px]">
             {movies.length > 0 ? (
               movies.map((data) => {
                 const releaseDate = new Date(data.theatrical_release);
                 const formattedDate = releaseDate.toISOString().split("T")[0];
                 return (
-                  <div
-                    className="flex flex-col gap-[16px] w-[161px] md:w-[285px]"
-                    key={data.id}
-                  >
+                  <div className="flex flex-col gap-[16px] " key={data.id}>
                     <img
-                      className="w-full h-full hover:border-[#81afe7]  hover:border hover:shadow-[0px_0px_60px_-25px_#81afe7] rounded"
+                      className="w-[161px] h-[235px] md:w-[285px] md:h-[416px] hover:border-[#81afe7]  hover:border hover:shadow-[0px_0px_60px_-25px_#81afe7] rounded"
                       src={data.image}
                       alt=""
                       onClick={() => {
@@ -115,21 +112,21 @@ function MovieList() {
                         {data.title}
                       </div>
                     </div>
-                    <div className="flex flex-col md:flex-row md:gap-[10px] gap-[10px]">
-                      <div className="grid gap-[8px] w-fit grid-cols-3 md:flex md:flex-row">
+                    <div className="flex flex-col md:flex-row md:gap-[10px] gap-[5px] ">
+                      <div className="grid gap-[8px] w-fit grid-cols-2 md:flex md:flex-row text-center">
                         {data.genres.map((genre) => {
                           return (
                             <div
-                              className="h-[32px] w-fit rounded-[4px] text-gray-300 bg-gray-100 px-[6px] py-[6px] max-[375px]:text-[14px]"
+                              className=" items-center md:w-fit rounded-[4px] text-gray-300 bg-gray-100 px-[6px] py-[6px] text-[14px]"
                               key={genre}
                             >
                               {genre}
                             </div>
                           );
                         })}
-                      </div>
-                      <div className="h-[32px] w-fit rounded-[4px] text-gray-400 bg-gray-100 px-[12px] py-[6px] text-[14px]">
-                        {data.language}
+                        <div className="h-[32px] w-fit rounded-[4px] text-gray-400 bg-gray-100 px-[12px] py-[6px] text-[14px]">
+                          {data.language}
+                        </div>
                       </div>
                     </div>
                   </div>
